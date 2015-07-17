1191c747d73cf127ee2d977bc9076950ff704aee
Victor van Poppelen <vvanpo@gmail.com>
2015-07-17 19:44:27 -0400

    main.c: Factored out option retrieval

--- main.c
+++ main.c
@@ -9,38 +9,20 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-struct option {
-    char *name;
-    char *value;
-};
-
 extern char **environ;
 extern char *program_name;
 
+char **get_options(char **);
+int client(char *, char *, char *);
+int server(char *, char *);
+
 int main(int argc, char **argv){
     char *program_name = basename(argv[0]);
-    struct option options[] = {
-        {"role"},
-        {"host"},
-        {"port"},
-        {"source"}
-    };
-    for (char **e = environ; *e; e++){
-        char *env = *e;
-        for (int i = 0; env[i] != '\0'; i++){
-            if (env[i] == '='){
-                for (int j = 0; j < sizeof(options)/sizeof(struct option); j++){
-                    if (!strncmp(options[j].name, env, i)
-                            && options[j].name[i] == '\0'){
-                        options[j].value = env + i + 1;
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-    }
-    int sd = socket(AF_INET, SOCK_DGRAM, 0);
+    // options is null-terminated so get_options knows how many there are.
+    char *options[] = { "role", "host", "port", "source", NULL };
+    enum option_index { ROLE, HOST, PORT, SOURCE };
+    char **option_values = get_options(options);
+/*    int sd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sd == -1)
         fprintf(stderr, "%s: socket() error: %s\n", program_name, strerror(errno));
     int port = atoi(options[2].value);
@@ -51,14 +33,47 @@ int main(int argc, char **argv){
     const struct sockaddr saddr = {
         AF_INET, htons(port), htons(address)
     };
-    if (!strcmp(options[0].value, "client")){
-        if (connect(sd, &saddr, sizeof(saddr))){
-            fprintf(stderr, "%s: connect() error: %s\n", program_name,
-                    strerror(errno));
-            close(sd);
+*/    if (!strcmp(options[ROLE], "client"))
+        return client(options[HOST], options[PORT], options[SOURCE]);
+    else if (!strcmp(options[ROLE], "server"))
+        return server(options[HOST], options[PORT]);
+}
+
+// get_options returns an array of option values, with the indices corresponding
+// to those of options.
+char **get_options(char **options){
+    char **values;
+    for (int i = 0;; i++)
+        if (!options[i]){
+            values = calloc(i, sizeof(char *));
+            break;
+        }
+    for (char **e = environ; *e; e++){
+        char *env = *e;
+        for (int i = 0; env[i] != '\0'; i++){
+            if (env[i] == '='){
+                for (int j = 0; options[j]; j++){
+                    if (!strncmp(options[j], env, i) && options[j][i] == '\0'){
+                        values[j] = env + i + 1;
+                        break;
+                    }
+                }
+                break;
+            }
         }
     }
-    else if (!strcmp(options[0].value, "server")){
-    }
+    return values;
+}
+
+int client(char *host, char *port, char *source){
+/*    if (connect(sd, &saddr, sizeof(saddr))){
+        fprintf(stderr, "%s: connect() error: %s\n", program_name,
+                strerror(errno));
+        close(sd);
+    }*/
+    return 0;
+}
+
+int server(char *host, char *port){
     return 0;
 }

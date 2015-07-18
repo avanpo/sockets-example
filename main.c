b0643b778d1836f3f0fdf2f0fd66d0c7b3f2e1de
Victor van Poppelen <vvanpo@gmail.com>
2015-07-18 15:10:44 -0400

    main.c: implemented bind_socket and recv_message

--- main.c
+++ main.c
@@ -23,8 +23,10 @@ struct socket {
 char **get_options();
 struct socket *open_socket(int);
 void close_socket(struct socket *);
+int bind_socket(struct socket *, char *);
 int get_address(struct sockaddr **, socklen_t *, char *, char *);
 int send_message(struct socket *, struct sockaddr *, socklen_t, char *);
+int recv_message(struct socket *);
 
 // external globals declarations
 extern char **environ;
@@ -32,8 +34,8 @@ extern char **environ;
 // static globals definitions
 char *program_name;
 // options is null-terminated so get_options knows how many there are
-char const * const options[] = { "host", "port", "source", "message", NULL };
-enum option_index { HOST, PORT, SOURCE, MESSAGE };
+char const * const options[] = { "host", "remote_port", "local_port", NULL };
+enum option_index { HOST, REMOTE_PORT, LOCAL_PORT };
 
 int main(int argc, char **argv){
     program_name = basename(argv[0]);
@@ -41,11 +43,9 @@ int main(int argc, char **argv){
     char **option_values = get_options();
     // Using a buffer length of 512 bytes to stay within the usual MTUs
     struct socket *sock = open_socket(512);
-    struct sockaddr *addr;
-    socklen_t addrlen;
-    if (get_address(&addr, &addrlen, option_values[HOST], option_values[PORT]))
+    if (bind_socket(sock, option_values[LOCAL_PORT]))
         return EXIT_FAILURE;
-    if (send_message(sock, addr, addrlen, option_values[MESSAGE]))
+    if (recv_message(sock))
         return EXIT_FAILURE;
     close_socket(sock);
     return EXIT_SUCCESS;
@@ -96,6 +96,7 @@ struct socket *open_socket(int buf_length){
     return sock;
 }
 
+// close_socket closes the passed socket and frees associated memory
 void close_socket(struct socket *sock){
     if (close(sock->fd)) error(close);
     free(sock->buffer);
@@ -103,7 +104,24 @@ void close_socket(struct socket *sock){
     return;
 }
 
+// bind_socket binds the socket to the specified local port if possible
+//  port can be a number or a service name from /etc/services
+//  passing NULL to port lets the kernel choose an ephemeral port to bind to
+int bind_socket(struct socket *sock, char *port){
+    struct sockaddr *addr;
+    socklen_t addrlen;
+    if (get_address(&addr, &addrlen, "localhost", port))
+        return -1;
+    if (bind(sock->fd, addr, addrlen)){
+        error(bind);
+        return -1;
+    }
+    return 0;
+}
+
 // get_address takes references to *addr and addrlen and populates them
+//  host can be a hostname, or an ip address
+//  port can be a number or a service name from /etc/services
 int get_address(struct sockaddr **addr, socklen_t *addrlen, char *host,
         char *port){
     struct addrinfo hints = { 0 };
@@ -126,6 +144,7 @@ int get_address(struct sockaddr **addr, socklen_t *addrlen, char *host,
     return 0;
 }
 
+// send_message sends a datagram to the specified remote address
 int send_message(struct socket *sock, struct sockaddr *addr, socklen_t addrlen,
         char *message){
     int length = strlen(message);
@@ -136,6 +155,20 @@ int send_message(struct socket *sock, struct sockaddr *addr, socklen_t addrlen,
     if (sendto(sock->fd, sock->buffer, length + 1, 0, addr, addrlen) == -1){
         error(sendto);
         return -1;
+    }
+    return 0;
+}
+
+// recv_message waits for an incoming datagram and writes it to sock->buffer
+//  if the socket is unbound, this call will cause the program to hang
+int recv_message(struct socket *sock){
+    int ret = recv(sock->fd, sock->buffer, sock->buf_length, MSG_TRUNC);
+    if (ret == -1){
+        error(recv);
+        return -1;
+    }
+    if (ret < sock->buf_length){
+        sock->buffer[ret] = '\0';
     }
     return 0;
 }

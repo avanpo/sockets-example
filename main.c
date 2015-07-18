e6659d0fb2741ca0d2064fcd8c16c72864cf75ee
Victor van Poppelen <vvanpo@gmail.com>
2015-07-18 00:11:04 -0400

    main.c: factored out address retrieval

--- main.c
+++ main.c
@@ -16,24 +16,19 @@
 extern char **environ;
 
 char **get_options();
+void get_address(struct sockaddr **, socklen_t *, char *, char *);
 int client(char **);
-int server(char **);
 
 char *program_name;
 // options is null-terminated so get_options knows how many there are.
-char const * const options[] = { "role", "host", "port", "source", NULL };
-enum option_index { ROLE, HOST, PORT, SOURCE };
+char const * const options[] = { "host", "port", "source", NULL };
+enum option_index { HOST, PORT, SOURCE };
 
 int main(int argc, char **argv){
     program_name = basename(argv[0]);
     // option_values are accessed as option_value[option_index].
     char **option_values = get_options();
-    if (!strcmp(option_values[ROLE], "client"))
-        return client(option_values);
-    else if (!strcmp(option_values[ROLE], "server"))
-        return server(option_values);
-    else
-        return EXIT_FAILURE;
+    return client(option_values);
 }
 
 // get_options returns an array of option values, with the indices corresponding
@@ -58,52 +53,38 @@ char **get_options(){
     return values;
 }
 
-int client(char **o_values){
-/*    if (connect(sd, &saddr, sizeof(saddr))){
-        fprintf(stderr, "%s: connect() error: %s\n", program_name,
-                strerror(errno));
-        close(sd);
-    }*/
-/*    int sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sd == -1)
-        fprintf(stderr, "%s: socket() error: %s\n", program_name, strerror(errno));
-    int port = atoi(options[2].value);
-    int address;
-    struct sockaddr const saddr = {
-        AF_INET, htons(port), htons(address)
-    };
-*/
+void get_address(struct sockaddr **addr, socklen_t *addrlen, char *host,
+        char *port){
     struct addrinfo hints = { 0 };
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
-    hints.ai_flags = AI_CANONNAME;
     struct addrinfo *result;
-    int gai_errno = getaddrinfo(o_values[HOST], o_values[PORT], &hints,
-            &result);
+    int gai_errno = getaddrinfo(host, port, &hints, &result);
     if (gai_errno == EAI_SYSTEM)
         error(getaddrinfo);
     else if (gai_errno)
         error_fmt(getaddrinfo, gai_strerror(gai_errno));
-    int sockfd;
-    for (struct addrinfo *cur = result; cur; cur = cur->ai_next){
-        sockfd = socket(cur->ai_family, cur->ai_socktype, cur->ai_protocol);
-        if (sockfd == -1){
-            if (!cur->ai_next)
-                error(socket);
-            continue;
-        }
-    }
+    *addr = result->ai_addr;
+    *addrlen = result->ai_addrlen;
     freeaddrinfo(result);
+    return;
+}
 
+int client(char **o_values){
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sockfd == -1)
+        error(socket);
+    struct sockaddr *addr;
+    socklen_t addrlen;
+    get_address(&addr, &addrlen, o_values[HOST], o_values[PORT]);
 
-
+    char *buffer = "testing transfer through UDP.\n";
+    if (sendto(sockfd, buffer, sizeof(buffer), 0, addr, addrlen) == -1)
+        error(sendto);
 
     if (close(sockfd))
         error(close);
     return EXIT_SUCCESS;
 }
 
-int server(char **o_values){
-    return EXIT_SUCCESS;
-}
